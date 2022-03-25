db2008afba2e95e6a4b2bb20e51a5a5cf9997f84
icefantik <43622554+icefantik@users.noreply.github.com>
2022-03-25 11:16:57 +0300

    Update Controller.java

--- src/main/java/com/example/todolist/Controller.java
+++ src/main/java/com/example/todolist/Controller.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+// Класс для работы со всеми записями
 public class Controller implements Initializable {
     private static ObservableList<Task> tasksData = FXCollections.observableArrayList(
             new Task(1, "Сделать toDoList", "Мне нужно сделать задание по инструменталкам", "24.02.22", "24.02.22")
@@ -34,6 +35,7 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Метод инициализации таблицы
         idColumn.setCellValueFactory(new PropertyValueFactory<Task, Integer>("Id"));
         titleColumn.setCellValueFactory(new PropertyValueFactory<Task, String>("Title"));
         descColumn.setCellValueFactory(new PropertyValueFactory<Task, String>("Desc"));
@@ -43,6 +45,7 @@ public class Controller implements Initializable {
     }
 
     public static void delId(int id) {
+        // Метод удаления по id
         for (int index = 0; index < tasksData.size(); ++index) {
             if (id == tasksData.get(index).id) {
                 delTasksData(index);
@@ -52,6 +55,7 @@ public class Controller implements Initializable {
 
     public static Task findOnIdElem(int id)
     {
+        // Метод нахождения элемента по id
         for (Task tasksDatum : tasksData) {
             if (id == tasksDatum.id) {
                 return new Task(tasksDatum.id, tasksDatum.title, tasksDatum.desc, tasksDatum.start, tasksDatum.due);
@@ -64,25 +68,25 @@ public class Controller implements Initializable {
     {
         tableTasks.setItems(tasksData);
     }
-
+    // метод добавления записи
     public static void addTasksData(Task task) {
         tasksData.add(task);
     }
-
+    // метод удаления записи
     public static void delTasksData(int index) {
         tasksData.remove(index);
     }
-
+    // метод открытия окна удаления записи
     public void openDelWindow(ActionEvent actionEvent) throws IOException {
         DelWindow delWindow = new DelWindow();
         delWindow.newWindow();
     }
-
+    // метод открытия окна редактирования записи
     public void openEditWindow(ActionEvent actionEvent) throws IOException {
         AddWindow addWindow = new AddWindow();
         addWindow.newWindow();
     }
-
+    // метод открывания окна для выбора id
     public void openGetIdWindow(ActionEvent actionEvent) throws IOException {
         GetIdWindow getIdWindow = new GetIdWindow();
         getIdWindow.newWindow();
